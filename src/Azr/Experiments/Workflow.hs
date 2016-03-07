f07a1b22ea25348d681a0cda91dcbb25463ebc3e
Greg Wiley <aztec.rex@jammm.com>
2016-03-06 20:19:47 -0800

    i think i may have a clue on this thing finally

--- src/Azr/Experiments/Workflow.hs
+++ src/Azr/Experiments/Workflow.hs
@@ -1,92 +1,84 @@
 
 module Azr.Experiments.Workflow where
 
+import Control.Monad.State.Strict
 
--- data Task a =
---   Dispatch String a
---   Return a
---
---
---
---
---
---
---
--- data Dispatch =
---
---
--- data Stat = Dispatched | Completed String
---
--- data Instruction = Instruction
---
--- run :: [Instruction] -> [Stat] -> ([Instruction], [Stat])
--- run = error "NYI"
---
---
---
+data Task a = Completed a | Dispatched String a deriving (Eq,Show)
 
-data Job a = Job a | Blocked deriving (Eq,Show)
+data Promise = Index Int deriving (Eq, Show)
 
-data Task a = Completed a | Dispatched deriving (Eq,Show)
+type Status = (Int, [Task Integer])
 
-dispatch :: String -> Integer -> Job (Task Integer)
-dispatch "double" x = Job . Completed $ 2 * x
-dispatch "triple" x = Job . Completed $ 3 * x
-dispatch "square" x = Job . Completed $ x * x
-dispatch _ _ = error "no such dispatch"
+update :: Task Integer -> Status -> Status
+update t (pos,ts) = (pos + 1, ts')
+  where ts' | length ts == pos = t : ts
+            | length ts > pos  = ts
+            | otherwise        = error $ "invariant violation " ++ show (pos,ts)
 
-require :: Task a -> Job a
-require (Completed a) = Job a
-require Dispatched = Blocked
+data Job a = Job a | Blocked [Task Integer] deriving (Eq,Show)
+
+type Program = StateT Status Job
+
+dispatch :: String -> Integer -> Program (Promise)
+dispatch name arg = do
+  idx <- gets fst
+  modify $ update (Dispatched name arg)
+  return $ Index idx
+
+
+require :: Promise -> Program Integer
+require (Index i) = do
+  task <- gets $ (!! i) . reverse . snd
+  case task of
+    (Dispatched _ _) -> do
+      (_,ts) <- get
+      lift $ Blocked ts
+    (Completed v) -> return v
 
 instance Functor Job where
-  fmap _ Blocked = Blocked
+  fmap _ (Blocked ts) = Blocked ts
   fmap f (Job x) = Job (f x)
 
 instance Applicative Job where
   pure = Job
-  Blocked <*> _ = Blocked
-  _ <*> Blocked = Blocked
+  Blocked ts <*> _ = Blocked ts
+  _ <*> Blocked ts = Blocked ts
   Job f <*> Job x = Job (f x)
 
 instance Monad Job where
-  Blocked >>= _ = Blocked
+  Blocked ts >>= _ = Blocked ts
   Job x >>= f = f x
 
-job :: Integer -> Job Integer
+job :: Integer -> Program Integer
 job x = do
   ta <- dispatch "double" x
   tb <- dispatch "triple" x
   a <- require ta
   tc <- dispatch "square" a
+  td <- dispatch "unknown" a
   c <- require tc
   b <- require tb
+  -- require td    <----- uncomment this to prevent completion
   return $ c + b
 
+initr :: Job Integer
+initr = Blocked []
 
+runJob :: Job Integer -> Job Integer
+runJob from = case from of
+  (Blocked ts) -> evalStateT (job 3) (0, fmap resolve ts)
+  _            -> from
 
--- data Task a = Completed a || Waiting
---
--- dispatch :: a -> Thing a
--- dispatch _ = Waiting
---
--- wait :: Task a -> Maybe a
--- wait Waiting = Nothing
--- wait (Completed x) = Just x
---
---
---
---
--- dispatch :: String -> Maybe String
--- fakeDispatch = error "dispatch is not a real thing"
---
--- fakeRequire :: String -> Maybe ()
--- fakeRequire = error "fake require is not a real thing"
---
--- fake :: Maybe String
--- fake = do
---     a <- fakeDispatch "message 1"
---     b <- fakeDispatch "message 2"
---     c <- fakeDispatch "message 3"
---     fakeRequire c
---     return $ a ++ b
+v1 = runJob initr
+v2 = runJob v1
+v3 = runJob v2
+v4 = runJob v3
+v5 = runJob v4
+
+resolve :: Task Integer -> Task Integer
+resolve (Dispatched op x) = case op of
+        "double" -> Completed $ 2 * x
+        "triple" -> Completed $ 3 * x
+        "square" -> Completed $ x * x
+        _        -> Dispatched op x
+resolve other = other
