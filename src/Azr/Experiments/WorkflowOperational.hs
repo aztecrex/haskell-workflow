801aa5bc323637d7f9beb5231eee0a44e54b0130
Greg Wiley <aztec.rex@jammm.com>
2016-03-13 11:00:18 -0700

    initial operational

--- src/Azr/Experiments/WorkflowOperational.hs
+++ src/Azr/Experiments/WorkflowOperational.hs
@@ -10,7 +10,11 @@ data WorkI t a r where
 
 type Work t a r = Program (WorkI t a) r
 
+dispatch :: t -> a -> Work t a (Maybe r)
+dispatch t a = singleton  (Dispatch t a)
 
+require :: Maybe r -> Work t a r
+require = singleton . Require
 
 -- type Job s a = Program (WorkI s) a
 
